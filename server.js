56eab0f4784a6aa04adcb5006ff5a01c82e1bab8
Aditya Jha <aditya.jha@indifi.com>
2017-05-08 22:46:13 +0530

    basic project changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const express = require('express');
+const express = require("express");
 const io = require("socket.io")();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
@@ -11,14 +11,25 @@ const app = express();
 app.io = io;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.set("views", path.join(__dirname, "public"));
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// send all requests to index so browserHistory in React Router works
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    const manifest = require("./manifest.json");
+
+    res.render("index", {
+        bundlePath: manifest.main.js.substring(8),
+        vendorPath: manifest.vendor.js.substring(8)
+    });
 });
 
 app.listen(4000, function () {
-   console.log("listening on http://localhost:4000");
+    console.log("listening on http://localhost:4000");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,49 @@
 
 "use strict";
 
-let config = {
-    devtool: 'cheap-module-source-map',
-    entry: [
-        './src/client/app.jsx'
-    ],
+const AssetsPlugin = require("assets-webpack-plugin");
+const webpack = require("webpack");
+
+const config = {
+    devtool: "cheap-module-source-map",
+    entry: "./src/client/app.jsx",
     output: {
         path: __dirname,
-        filename: './public/js/bundle.js'
+        filename: "./public/js/[name]-[chunkhash].js"
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: [".js", ".jsx"]
     },
     module: {
         loaders: [
             {
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 query: {
-                    presets: ['react', 'es2015', 'stage-0']
+                    presets: ["react", "es2015", "stage-0"]
                 },
                 test: /\.jsx?$/,
                 exclude: /(node_module|bower_components)/
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader?modules",
+                include: /flexboxgrid/
             }
         ]
     },
-    plugins: []
+    plugins: [
+        new AssetsPlugin({
+            filename: "manifest.json",
+            prettyPrint: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf("node_modules") !== -1;
+            }
+        })
+    ]
 };
 
 module.exports = config;
